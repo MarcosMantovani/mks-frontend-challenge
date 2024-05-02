ddfedba247a52af19a84c243730bbea03ff2f848
Marcos Vinicius Mantovani <marcospiderplays@gmail.com>
2024-05-02 15:48:07 -0300

    feat: footer component

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { colors } from '../../styles'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  padding: 8px 0;
+  background-color: ${colors.footerWhite};
+
+  font-size: 12px;
+`
