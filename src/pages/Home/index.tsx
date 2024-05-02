75b7e16cc9362bfa51a19abc5acfcb31d3d63335
Marcos Vinicius Mantovani <marcospiderplays@gmail.com>
2024-05-02 15:49:04 -0300

    feat: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,24 @@
+import ProductsList from '../../components/ProductsList'
+import { useGetProductsQuery } from '../../services/api'
+
+import * as S from './styles'
+
 const Home = () => {
-  return <div className="container">Home</div>
+  const { data: productsResponse, isLoading } = useGetProductsQuery()
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (productsResponse) {
+    return (
+      <S.Container className="container">
+        <ProductsList products={productsResponse.products} />
+      </S.Container>
+    )
+  }
+
+  return <p>Sem data...</p>
 }
 
 export default Home
