126fd1936b2289e23df3258c56ac17f0aba9f775
Marcos Vinicius Mantovani <marcospiderplays@gmail.com>
2024-05-02 15:51:01 -0300

    feat: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
+
 import PageRoutes from './routes'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Cart from './components/Cart'
+
 import { GlobalCss } from './styles'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <PageRoutes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <Header />
+        <PageRoutes />
+        <Footer />
+        <Cart />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
