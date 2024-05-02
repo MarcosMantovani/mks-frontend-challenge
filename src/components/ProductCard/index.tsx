4ceafef6f81bc1c27a507f1485d99557db0dc3d5
Marcos Vinicius Mantovani <marcospiderplays@gmail.com>
2024-05-02 19:24:26 -0300

    feat: hover and button product card animation

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -3,8 +3,12 @@ import { useDispatch } from 'react-redux'
 import { add } from '../../store/reducers/cart'
 
 import shoppingBag from '../../assets/images/shopping-bag.svg'
+import plusIcon from '../../assets/images/plus-outline.svg'
 
 import * as S from './styles'
+import { useCheckProductInCart } from '../../utils'
+import { colors } from '../../styles'
+import { motion } from 'framer-motion'
 
 type Props = {
   product: Product
@@ -22,18 +26,38 @@ const ProductCard = ({ product }: Props) => {
 
   const addToCart = () => dispatch(add(product))
 
+  const isProductInCart = useCheckProductInCart(product.id)
+
   return (
-    <S.Card>
+    <S.Card whileHover={{ scale: 1.1 }}>
       <div className="content">
-        <S.Photo src={product.photo} alt={product.name} />
+        <S.Photo>
+          <img className="image" src={product.photo} alt={product.name} />
+        </S.Photo>
         <S.MainInfo>
           <p className="name">{product.name}</p>
           <span className="price">R${Math.trunc(product.price)}</span>
         </S.MainInfo>
         <S.Description>{getDescription(product.description)}</S.Description>
       </div>
-      <S.Button onClick={addToCart}>
-        <img src={shoppingBag} alt="Shopping bag" />
+      <S.Button
+        onClick={addToCart}
+        animate={{
+          backgroundColor: isProductInCart
+            ? `${colors.priceGray}`
+            : `${colors.blue}`
+        }}
+        whileHover={{ backgroundColor: `${colors.black}` }}
+        transition={{ duration: 0.2 }}
+      >
+        <motion.img
+          className="icon"
+          src={isProductInCart ? plusIcon : shoppingBag}
+          alt="Shopping bag"
+          initial={{ opacity: 0 }} // define a opacidade inicial como 0
+          animate={{ opacity: 1 }} // define a opacidade final como 1
+          transition={{ duration: 0.5 }} // define a duração da transição
+        />
         <p className="priceText">Comprar</p>
       </S.Button>
     </S.Card>
